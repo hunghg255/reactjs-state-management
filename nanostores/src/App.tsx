912e7ddaff63c6a63ef6e769ea11f375bb164289
hunghg255 <giahunghust@gmail.com>
2023-04-30 20:14:14 +0700

    update

---
+++ nanostores/src/App.tsx
@@ -0,0 +1,78 @@
+import { atom, } from 'nanostores';
+import { useStore } from '@nanostores/react';
+
+const atomValue = atom({
+  first: '',
+  last: '',
+});
+
+const TextInput = ({ value }: { value: 'first' | 'last' }) => {
+  const fieldValue = useStore(atomValue);
+
+  return (
+    <div className='field'>
+      {value}:{' '}
+      <input
+        value={fieldValue[value]}
+        onChange={(e) =>
+          atomValue.set({ ...fieldValue, [value]: e.target.value || '' })
+        }
+      />
+    </div>
+  );
+};
+
+const Display = ({ value }: { value: 'first' | 'last' }) => {
+  const fieldValue = useStore(atomValue)
+
+  return (
+    <div className='value'>
+      {value}: {fieldValue[value]}
+    </div>
+  );
+};
+
+const FormContainer = () => {
+  return (
+    <div className='container'>
+      <h5>FormContainer</h5>
+      <TextInput value='first' />
+      <TextInput value='last' />
+    </div>
+  );
+};
+
+const DisplayContainer = () => {
+  return (
+    <div className='container'>
+      <h5>DisplayContainer</h5>
+      <Display value='first' />
+      <Display value='last' />
+    </div>
+  );
+};
+
+const ContentContainer = () => {
+  return (
+    <div className='container'>
+      <h5>ContentContainer</h5>
+      <FormContainer />
+      <DisplayContainer />
+    </div>
+  );
+};
+
+const Title = () => {
+  return <h5>App</h5>
+}
+
+function App() {
+  return (
+      <div className='container'>
+        <Title />
+        <ContentContainer />
+      </div>
+  );
+}
+
+export default App;
